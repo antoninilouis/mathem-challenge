cf24b43ffdda23c3090ae35431468bd3705665c1
Louis Antonini <louis.antonini@epitech.eu>
2020-05-07 15:00:37 +0200

    Create core class Product to include in orders

---
+++ src/main/java/mathem/challenge/App.java
@@ -0,0 +1,22 @@
+package mathem.challenge;
+
+
+// Also include a hard-coded definition for “green” (environment-friendly) delivery dates. This could
+// be as simple as “all Wednesdays” or “date 5, 15 and 25 each month”.
+
+/**
+ * - A delivery date is not valid if a product can't be delivered on that
+ *   weekday
+ * - A delivery date is not valid if a product must be ordered more days in 
+ *   advance than possible
+ * - All external products need to be ordered 5 days in advance
+ * - Temporary products can only be ordered within the current week (Mon-Sun)
+ * 
+ * The result should be sorted in priority order, with green delivery dates at
+ * the top of the list if they are within the next 3 days, otherwise dates
+ * should just be sorted ascending.
+ */
+public class App {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/mathem/challenge/Product.java
@@ -0,0 +1,100 @@
+package mathem.challenge;
+
+import java.io.UnsupportedEncodingException;
+import java.time.DayOfWeek;
+import java.util.EnumSet;
+import java.util.UUID;
+
+/**
+ * - productId
+ * - name
+ * - deliveryDays (a list of weekdays when the product can be delivered)
+ * - productType (normal, external or temporary)
+ * - daysInAdvance (how many days before delivery the products need to be
+ *   ordered)
+ */
+public class Product {
+    private final UUID productId;
+    private final String productName;
+    private final ProductType productType;
+    private EnumSet<DayOfWeek> deliveryDays;
+    private int daysInAdvance;
+
+    public static final String MATHEM_NAMESPACE = "mathem.se";
+    public static enum ProductType { NORMAL, EXTERNAL, TEMPORARY };
+
+    /**
+     * Constructor for Product object with all deliveryDays and default
+     * days in advance constraint.
+     * @param productId - the UUID (type 3) of the product
+     * @param productName - the name of the product
+     * @param productType - the type of the product
+     */
+    private Product(UUID productId, String productName,
+                    ProductType productType) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productType = productType;
+        this.deliveryDays = EnumSet.allOf(DayOfWeek.class);
+        this.daysInAdvance = 0;
+    };
+
+    /**
+     * Constructor for Product object with specified deliveryDays and days in
+     * advance constraint.
+     * @param productId - the UUID (type 3) of the product
+     * @param productName - the name of the product
+     * @param productType - the type of the product
+     * @param deliveryDays - the delivery days constraints of the product
+     * @param daysInAdvance - the days in advance constraints of the product
+     */
+    private Product(UUID productId, String productName,
+                    ProductType productType, EnumSet<DayOfWeek> deliveryDays,
+                    int daysInAdvance) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productType = productType;
+        this.deliveryDays = deliveryDays;
+        this.daysInAdvance = daysInAdvance;
+    };
+
+    /**
+     * Static factory method for products with default constraints
+     * @param name - the name of the product
+     * @param productType - the type of the product
+     * @return an object of class Product initialized by the constructor for
+     * Product objects with all deliveryDays and default
+     */
+    public static Product create(String name, ProductType productType) {
+        try {
+            byte[] bytes = (MATHEM_NAMESPACE + name).getBytes("UTF-8");
+            UUID uuid = UUID.nameUUIDFromBytes(bytes);
+            return new Product(uuid, name, productType);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Static factory method for products with constraints
+     * @param name - the name of the product
+     * @param productType - the type of the product
+     * @param deliveryDays - the delivery days constraints of the product
+     * @param daysInAdvance - the days in advance constraints of the product
+     * @return an object of class Product initialized by the constructor for
+     * Product objects with all deliveryDays and default
+     */
+    public static Product create(String name, ProductType productType,
+                                 EnumSet<DayOfWeek> deliveryDays,
+                                 int daysInAdvance) {
+        try {
+            byte[] bytes = (MATHEM_NAMESPACE + name).getBytes("UTF-8");
+            UUID uuid = UUID.nameUUIDFromBytes(bytes);
+            return new Product(uuid, name, productType, deliveryDays, daysInAdvance);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
